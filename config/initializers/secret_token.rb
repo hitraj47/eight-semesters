1cc9467dcf0ea0b81c6f9ebfd4e5c448bc651dec
Tacksoo Im <tacksoo.im@gmail.com>
2013-05-16 22:33:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EightSemesters::Application.config.secret_token = '6201165eda07691dd947e3440ea6ed1b990467f6d1aef970952997c81a17fa23cd244c458cd3fecfa9a0298db5ddd243b38e5bfdb2c3111f6b733e96743ccbaa'
